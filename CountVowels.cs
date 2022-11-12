99895dd2af5b4d7539c543f4a8532c1b594f9f66
Sarmat <xonely@icloud.com>
2022-11-12 14:25:38 +0300

    Create CountVowels.cs

---
+++ CountVowels.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите слово, чтобы подсчитать количество гласных");
+char[] input = Console.ReadLine().ToLower().ToCharArray();
+int CountVowels()
+{
+    int counting = 0;
+    char[] vowels = { 'а', 'о', 'у', 'э', 'ы', 'я', 'ё', 'ю', 'е', 'и', };
+    foreach (char inputCh in input)
+    {
+        foreach (char vowel in vowels)
+        {
+            if (inputCh == vowel) { counting++; }
+        }
+    }
+    return counting;
+}
+Console.WriteLine(CountVowels());
+Console.ReadKey();
+
+
+
